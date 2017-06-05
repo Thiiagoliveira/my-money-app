741905b4f486e44830e6a5436e24a00a8f4a25b7
Thiago Oliveira <thiago.bsb@hotmail.com>
2017-06-04 23:57:50 -0300

    my-money.v3

---
+++ backend/src/config/cors.js
@@ -0,0 +1,8 @@
+// **Cross-Origin Resource Sharing** - É como se fosse Requisições de origem distintas;
+
+module.exports = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTION, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+}
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,3 +2,10 @@ const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
 
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigado."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é menor que o limite máximo."
+mongoose.Error.messages.String.enum = "'{VALUE}' não é valido ao atributo '{PATH}'."
+
+
+
