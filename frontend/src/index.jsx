fa5f75b3ff5e928ad3a78a77875bea1fc7380595
Thiago.santos <thiago.bsb@hotmail.com>
2017-06-05 16:53:03 -0300

    my-money.v7

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { applyMiddleware, createStore } from 'redux'
-import { Provider } from 'react-redux'
-import promise from 'redux-promise'
-import App from './main/app'
-import reducers from './main/reducers'
+import React from "react";
+import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import promise from "redux-promise";
+import App from "./main/app";
+import reducers from "./main/reducers";
 
-const store = applyMiddleware(promise)(createStore)(reducers)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = applyMiddleware(promise)(createStore)(reducers, devTools);
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider >
-    , document.getElementById('app')
-)
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
 import DashboardReducer from '../Dashboard/dashboardReducer'
-
+import TabReducer from '../common/tab/tabReducer'
 const rootReducer = combineReducers({
     dashboard: DashboardReducer
 })
