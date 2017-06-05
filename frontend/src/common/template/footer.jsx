9e55c6307c6309acd04a8b02c34fa31ad57c1b1c
Thiago Oliveira <thiago.bsb@hotmail.com>
2017-06-05 10:43:33 -0300

    my-money.v5

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer id='footer' className='main-footer'>
+        <strong>
+            Copyright &copy; 2017
+            <a href='http://google.com.br' target='_blank'> Google</a>.
+      </strong>
+    </footer>
+)
