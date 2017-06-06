96f1a82483c423e6e8a3313dd5fbe3e0f2247eb5
Thiago.santos <thiago.bsb@hotmail.com>
2017-06-06 16:45:56 -0300

    my-app.v9

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -9,49 +9,51 @@ import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 import { selectTab, showTabs } from '../common/tab/tabActions'
-
+import { create } from './billingCycleActions'
+import List from './billingCycleList'
+import Form from './billingCycleForm'
 class BillingCycle extends Component {
-  componentWillMount() {
-    this.props.selectTab('tabList')
-    this.props.showTabs('tabList', 'tabCreate')
-  }
-  render() {
-    return (
-      <div>
-        <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
-        <Content>
-          <Tabs>
-            <TabsHeader>
-              <TabHeader label='Listar' icon='bars'
-                target='tabList' />
-              <TabHeader label='Incluir' icon='plus'
-                target='tabCreate' />
-              <TabHeader label='Alterar' icon='pencil'
-                target='tabUpdate' />
-              <TabHeader label='Excluir' icon='trash-o'
-                target='tabDelete' />
-            </TabsHeader>
-            <TabsContent>
-              <TabContent id='tabList'>
-                <h1>Lista</h1>
-              </TabContent>
-              <TabContent id='tabCreate'>
-                <h1>Incluir</h1>
-              </TabContent>
-              <TabContent id='tabUpdate'>
-                <h1>Alterar</h1>
-              </TabContent>
-              <TabContent id='tabDelete'>
-                <h1>Excluir</h1>
-              </TabContent>
-            </TabsContent>
-          </Tabs>
-        </Content>
-      </div>
-    )
-  }
+  componentWillMount() {
+  this.props.selectTab('tabList')
+  this.props.showTabs('tabList', 'tabCreate')
 }
-
-const mapDispatchToProps = dispatch => bindActionCreators({ selectTab, showTabs },
-  dispatch)
+  render() {
+  return (
+  <div>
+  <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
+  <Content>
+  <Tabs>
+  <TabsHeader>
+  <TabHeader label='Listar' icon='bars'
+target='tabList' />
+  <TabHeader label='Incluir' icon='plus'
+target='tabCreate' />
+  <TabHeader label='Alterar' icon='pencil'
+target='tabUpdate' />
+  <TabHeader label='Excluir' icon='trash-o'
+target='tabDelete' />
+  </TabsHeader>
+  <TabsContent>
+  <TabContent id='tabList'>
+  <List />
+  </TabContent>
+  <TabContent id='tabCreate'>
+  <Form onSubmit={this.props.create} />
+  </TabContent>
+  <TabContent id='tabUpdate'>
+  <h1>Alterar</h1>
+  </TabContent>
+  <TabContent id='tabDelete'>
+  <h1>Excluir</h1>
+  </TabContent>
+  </TabsContent>
+  </Tabs>
+  </Content>
+  </div>
+  )
+  }
+}
+const mapDispatchToProps = dispatch => bindActionCreators({
+  selectTab, showTabs, create
+}, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
--- frontend/src/common/tab/tabs.jsx
+++ frontend/src/common/tab/tabs.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 export default props => (
   <div className="nav-tabs-custom">
      {props.children}
